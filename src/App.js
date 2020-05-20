06235f17285d896ab29d47263e360f277100a886
Nimit Haria <nimitharia007@gmail.com>
2020-05-21 00:00:12 +0530

    react-first

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">
+            Ristorante Con Fusion
+          </NavbarBrand>
+        </div>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
